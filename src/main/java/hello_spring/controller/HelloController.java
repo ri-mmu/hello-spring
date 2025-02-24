f6eb86a3425cba226455cba99dc72ceb25c316fb
ri-mmu <kkjjrr0504@gmail.com>
2025-02-24 17:52:54 +0900

    스프링 입문

---
+++ src/main/java/hello_spring/controller/HelloController.java
@@ -0,0 +1,49 @@
+package hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http 헤더랑 바디 중 바디에 직접 넣어주겠다 (소스 보기 하면 차이점이 보임)
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체를 받으면 Json 방식
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
---
+++ src/main/java/hello_spring/controller/MemberFrom.java
@@ -0,0 +1,14 @@
+package hello_spring.controller;
+
+public class MemberFrom {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
